2698245a8210199c4d56113abe3e632b36da143c
Rifafauziah1 <rifaf233@gmail.com>
2021-12-24 03:47:43 -0600

    role akses

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,23 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+
+    Dashboard
+
+@stop
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+    Welcome to this beautiful admin panel.
 
-                        @role('admin')
-                            Ini Untuk Admin
-                        @endrole
+@stop
 
-                        @role('pengguna')
-                            Ini Untuk Pengguna
-                        @endrole
+@section('css')
 
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
+@stop
+
+@section('js')
+
+@stop
